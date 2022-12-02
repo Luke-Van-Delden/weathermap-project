86b174542fa015ce34dd842aff1a95bafa0512cb
Luke Van Delden <vandelden.luke@gmail.com>
2022-12-01 22:02:08 -0600

    added marker add and drag functionality

--- js/main.js
+++ js/main.js
@@ -24,29 +24,29 @@ $.get("http://api.openweathermap.org/data/2.5/forecast", {
 
 // Function to generate info for current day
 function displayInfoCurrent(data) {
-    $('#current').html('<p>Current: Temp ' + data.main.temp + 'F. Weather: ' + data.weather[0].main + ' Humidity: ' + data.main.humidity + ' Wind: ' + data.wind.speed + 'mph Pressure: ' + data.main.pressure +  '</p>')
-    $('#current').html('<div class="row d-flex space-between flex-wrap divbox mb-3 mt-3"><div class="d-flex justify-content-center bg-secondary flex-grow-1">Current</div><div class="d-flex flex-grow-1 justify-content-center"> Temp: ' + data.main.temp + 'F</div><div class="d-flex flex-grow-1 justify-content-center"> Weather: ' + data.weather[0].main +  '</div><div class="d-flex justify-content-center"><img src="http://openweathermap.org/img/w/' + data.weather[0].icon + '.png"></div><div class="d-flex justify-content-center">Humidity: ' + data.main.humidity + '</div><div class="d-flex justify-content-center">Wind: ' + data.wind.speed + 'mph</div><div class="d-flex justify-content-center"> Pressure: ' + data.main.pressure +  '</div></div>')
+    $('#current').html('<p>Current: Temp ' + data.main.temp + 'F. Weather: ' + data.weather[0].main + ' Humidity: ' + data.main.humidity + ' Wind: ' + data.wind.speed + 'mph Pressure: ' + data.main.pressure + '</p>')
+    $('#current').html('<div class="row d-flex space-between flex-wrap divbox mb-3 mt-3"><div class="d-flex justify-content-center bg-secondary flex-grow-1">Current</div><div class="d-flex flex-grow-1 justify-content-center"> Temp: ' + data.main.temp + 'F</div><div class="d-flex flex-grow-1 justify-content-center"> Weather: ' + data.weather[0].main + '</div><div class="d-flex justify-content-center border-bottom"><img src="http://openweathermap.org/img/w/' + data.weather[0].icon + '.png"></div><div class="d-flex justify-content-center border-bottom">Humidity: ' + data.main.humidity + '</div><div class="d-flex justify-content-center border-bottom">Wind: ' + data.wind.speed + 'mph</div><div class="d-flex justify-content-center"> Pressure: ' + data.main.pressure + '</div></div>')
 
 }
 
 // Function to generate info per day
 function displayInfoForecast(data) {
-        if (data.dt_txt.indexOf("15:00:00") !== -1)
-    {
-        let test = $('#forecast').append('<div class="row d-flex space-between flex-wrap divbox"><div class="d-flex justify-content-center bg-secondary flex-grow-1">Date: ' + data.dt_txt + '</div><div class="d-flex flex-grow-1 justify-content-center"> Temp: ' + data.main.temp + 'F</div><div class="d-flex flex-grow-1 justify-content-center"> Weather: ' + data.weather[0].main +  '</div><div class="d-flex justify-content-center"><img src="http://openweathermap.org/img/w/' + data.weather[0].icon + '.png"></div><div class="d-flex justify-content-center">Humidity: ' + data.main.humidity + '</div><div class="d-flex justify-content-center">Wind: ' + data.wind.speed + 'mph</div><div class="d-flex justify-content-center"> Pressure: ' + data.main.pressure +  '</div></div>')
+    if (data.dt_txt.indexOf("15:00:00") !== -1) {
+        let test = $('#forecast').append('<div class="row d-flex space-between flex-wrap divbox"><div class="d-flex justify-content-center bg-secondary flex-grow-1">Date: ' + data.dt_txt + '</div><div class="d-flex flex-grow-1 justify-content-center"> Temp: ' + data.main.temp + 'F</div><div class="d-flex flex-grow-1 justify-content-center"> Weather: ' + data.weather[0].main + '</div><div class="d-flex justify-content-center border-bottom"><img src="http://openweathermap.org/img/w/' + data.weather[0].icon + '.png"></div><div class="d-flex justify-content-center border-bottom">Humidity: ' + data.main.humidity + '</div><div class="d-flex justify-content-center border-bottom">Wind: ' + data.wind.speed + 'mph</div><div class="d-flex justify-content-center"> Pressure: ' + data.main.pressure + '</div></div>')
     }
 }
+
 // Takes in input of search, stores as searchFor
-$('#searchbtn').click(function(e){
+$('#searchbtn').click(function (e) {
     e.preventDefault();
     let searchFor = $('#addresssearch').val();
     $.get("http://api.openweathermap.org/data/2.5/weather", {
         APPID: weatherKey,
-        q:     searchFor + ", US",
+        q: searchFor + ", US",
         units: "imperial"
-    }).done(function(data) {
+    }).done(function (data) {
         let original = $('#forecast').html()
-        if (original !== ""){
+        if (original !== "") {
             $('#forecast').html("")
         }
         displayInfoCurrent(data);
@@ -61,10 +61,80 @@ $('#searchbtn').click(function(e){
 })
 
 // TODO: DONE -Creates map zoomed out, centered on San Antonio
-// mapboxgl.accessToken = mapBoxKey;
-// var map = new mapboxgl.Map({
-//     container: 'map',
-//     style: 'mapbox://styles/mapbox/streets-v9',
-//     zoom: 10,
-//     center: [-98.48527, 29.423017],
-// });
+mapboxgl.accessToken = mapKey;
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    zoom: 10,
+    center: [-98.48527, 29.423017],
+});
+// Initialize the GeolocateControl.
+const geolocate = new mapboxgl.GeolocateControl({
+    positionOptions: {
+        enableHighAccuracy: true
+    },
+    trackUserLocation: true
+});
+// Add the control to the map.
+map.addControl(geolocate);
+// Set an event listener that fires
+// when a geolocate event occurs.
+geolocate.on('geolocate', () => {
+    console.log('A geolocate event has occurred.');
+});
+// Example of a MapMouseEvent of type "click"
+map.on('click', (e) => {
+    console.log(e);
+    console.log(e.lngLat)
+    const marker = new mapboxgl.Marker({
+        draggable: true
+    })
+        .setLngLat(e.lngLat)
+        .addTo(map);
+    $.get("http://api.openweathermap.org/data/2.5/weather", {
+        APPID: weatherKey,
+        lat: e.lngLat.lat,
+        lon: e.lngLat.lng,
+        units: "imperial"
+    }).done(function (data) {
+        let original = $('#forecast').html()
+        if (original !== "") {
+            $('#forecast').html("")
+        }
+        displayInfoCurrent(data);
+    });
+    $.get("http://api.openweathermap.org/data/2.5/forecast", {
+        APPID: weatherKey,
+        lat: e.lngLat.lat,
+        lon: e.lngLat.lng,
+        units: "imperial"
+    }).done(function (data) {
+        data.list.forEach(displayInfoForecast);
+    });
+
+    function onDragEnd() {
+        const lngLat = marker.getLngLat();
+        $.get("http://api.openweathermap.org/data/2.5/weather", {
+            APPID: weatherKey,
+            lat: lngLat.lat,
+            lon: lngLat.lng,
+            units: "imperial"
+        }).done(function (data) {
+            let original = $('#forecast').html()
+            if (original !== "") {
+                $('#forecast').html("")
+            }
+            displayInfoCurrent(data);
+        });
+        $.get("http://api.openweathermap.org/data/2.5/forecast", {
+            APPID: weatherKey,
+            lat: lngLat.lat,
+            lon: lngLat.lng,
+            units: "imperial"
+        }).done(function (data) {
+            data.list.forEach(displayInfoForecast);
+        });
+    }
+
+    marker.on('dragend', onDragEnd)
+});
